7a443b09e151cbbac821d9792c0e24d3401469ae
Nick Walker <nick.walker@puppetlabs.com>
2016-06-07 15:44:48 -0700

    update migration pre and post scripts

--- files/migration_restore_post_install.sh
+++ files/migration_restore_post_install.sh
@@ -14,51 +14,13 @@ sudo -u pe-postgres /opt/puppetlabs/server/bin/pg_restore -Cc $BACKUP_DIR/pe-cla
 sudo -u pe-postgres /opt/puppetlabs/server/bin/pg_restore -Cc $BACKUP_DIR/pe-activity.backup.bin -d template1
 sudo -u pe-postgres /opt/puppetlabs/server/bin/pg_restore -Cc $BACKUP_DIR/pe-rbac.backup.bin -d template1
 
-echo 'REVOKE ALL ON DATABASE "pe-activity" FROM PUBLIC;
-REVOKE ALL ON DATABASE "pe-activity" FROM "pe-postgres";
-GRANT ALL ON DATABASE "pe-activity" TO "pe-postgres";
-GRANT TEMPORARY ON DATABASE "pe-activity" TO PUBLIC;
-GRANT ALL ON DATABASE "pe-activity" TO "pe-activity";
-REVOKE ALL ON DATABASE "pe-classifier" FROM PUBLIC;
-REVOKE ALL ON DATABASE "pe-classifier" FROM "pe-postgres";
-GRANT ALL ON DATABASE "pe-classifier" TO "pe-postgres";
-GRANT TEMPORARY ON DATABASE "pe-classifier" TO PUBLIC;
-GRANT ALL ON DATABASE "pe-classifier" TO "pe-classifier";
-REVOKE ALL ON DATABASE "pe-puppetdb" FROM PUBLIC;
-REVOKE ALL ON DATABASE "pe-puppetdb" FROM "pe-postgres";
-GRANT ALL ON DATABASE "pe-puppetdb" TO "pe-postgres";
-GRANT TEMPORARY ON DATABASE "pe-puppetdb" TO PUBLIC;
-GRANT ALL ON DATABASE "pe-puppetdb" TO "pe-puppetdb";
-REVOKE ALL ON DATABASE "pe-rbac" FROM PUBLIC;
-REVOKE ALL ON DATABASE "pe-rbac" FROM "pe-postgres";
-GRANT ALL ON DATABASE "pe-rbac" TO "pe-postgres";
-GRANT TEMPORARY ON DATABASE "pe-rbac" TO PUBLIC;
-GRANT ALL ON DATABASE "pe-rbac" TO "pe-rbac";
-REVOKE ALL ON DATABASE template1 FROM PUBLIC;
-REVOKE ALL ON DATABASE template1 FROM "pe-postgres";
-GRANT ALL ON DATABASE template1 TO "pe-postgres";
-GRANT CONNECT ON DATABASE template1 TO PUBLIC;' > $BACKUP_DIR/grant_perms.sql
-
-sudo -u pe-postgres /opt/puppetlabs/server/bin/psql -f $BACKUP_DIR/grant_perms.sql
-
-echo '\c pe-puppetdb;
-CREATE EXTENSION pg_trgm;
-CREATE EXTENSION pgcrypto;' > $BACKUP_DIR/recreate_extensions.sql
-
-sudo -u pe-postgres /opt/puppetlabs/server/bin/psql -f $BACKUP_DIR/recreate_extensions.sql
-
-puppet resource service pe-puppetserver ensure=running
-puppet resource service pe-puppetdb ensure=running
-puppet resource service pe-console-services ensure=running
-puppet resource service pe-nginx ensure=running
-puppet resource service pe-activemq ensure=running
-puppet resource service pe-orchestration-services ensure=running
-puppet resource service pxp-agent ensure=running
+#Start PE services
+#Install database extensions and repair database permissions
+#Repair PE classification groups to reflect the 2016.2 Puppet master’s certificate name.
+puppet enterprise configure --modulepath /opt/puppetlabs/server/data/enterprise/modules
 
 #Optional: For cleaniness revoke cert from old master if certname is different
 #OLD_MASTER_CERTNAME=<old_master_certname_here>
 #puppet node deactivate $OLD_MASTER_CERTNAME
 #puppet cert clean $OLD_MASTER_CERTNAME
 
-#This step should repair the Node Classifier groups from old hostname to new hostname
-puppet enterprise configure --modulepath /opt/puppetlabs/server/data/enterprise/modules
--- files/migration_restore_pre_install.sh
+++ files/migration_restore_pre_install.sh
@@ -1,6 +1,18 @@
 BACKUP_DIR=/vagrant/files/backup
 
 mkdir -p /etc/puppetlabs/puppet
-cd /
 
-tar -zxvf $BACKUP_DIR/puppet_ssl.tar.gz
+tar -zxvf $BACKUP_DIR/puppet_ssl.tar.gz -C /
+
+#Removing PE internal certs so they can be regenerated
+rm -f /etc/puppetlabs/puppet/ssl/certs/pe-internal-classifier.pem
+rm -f /etc/puppetlabs/puppet/ssl/certs/pe-internal-dashboard.pem
+rm -f /etc/puppetlabs/puppet/ssl/private_keys/pe-internal-classifier.pem
+rm -f /etc/puppetlabs/puppet/ssl/private_keys/pe-internal-dashboard.pem
+rm -f /etc/puppetlabs/puppet/ssl/public_keys/pe-internal-classifier.pem
+rm -f /etc/puppetlabs/puppet/ssl/public_keys/pe-internal-dashboard.pem
+rm -f /etc/puppetlabs/puppet/ssl/ca/signed/pe-internal-classifier.pem
+rm -f /etc/puppetlabs/puppet/ssl/ca/signed/pe-internal-dashboard.pem
+
+#THIS IS A TEMPORARY HACK
+rm -f /etc/puppetlabs/puppet/ssl/certs/ca.pem
